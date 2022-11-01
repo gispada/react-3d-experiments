291374c443ba784e78c6cf8ab96a670d208c03b3
Gianluca Spada <gianluca.spada@stackhouse.it>
2022-11-01 17:33:27 +0100

    Constrain camera movement to ship's deck - work in progress

--- src/pages/ShipScene3D/WaterScene.tsx
+++ src/pages/ShipScene3D/WaterScene.tsx
@@ -1,15 +1,23 @@
 import { useGLTF, useKeyboardControls } from '@react-three/drei'
 import { extend, useFrame, useThree } from '@react-three/fiber'
 import { useLayoutEffect, useRef } from 'react'
-import { Euler, Quaternion, Vector3, Camera } from 'three'
+import { Euler, Quaternion, Vector3, Camera, Raycaster, Object3D } from 'three'
 import { GerstnerWater } from './GerstnerWater'
 
 type Props = {
   sunDirection: Vector3
+  onBoardCamera?: boolean
 }
 
 extend({ GerstnerWater })
 
+const WALKING_SURFACE = 'Object_21' // The ship's deck. TODO: create a separate mesh for this
+const CHARACTER_HEIGHT = 1.7
+
+const raycaster = new Raycaster()
+const raycasterDirection = new Vector3(0, -1, 0) // Looking down
+
+const worldPosition = new Vector3()
 const localDirection = new Vector3()
 
 const getCameraLocalDirection = (camera: Camera, target: Vector3) => {
@@ -18,22 +26,30 @@ const getCameraLocalDirection = (camera: Camera, target: Vector3) => {
   target.set(-e[8], -e[9], -e[10]).normalize()
 }
 
-export const WaterScene = ({ sunDirection }: Props) => {
+export const WaterScene = ({ sunDirection, onBoardCamera }: Props) => {
   const { scene: ship } = useGLTF('3d-models/frigate/scene.gltf')
+
   const [, get] = useKeyboardControls()
-  const { camera } = useThree()
+  const camera = useThree((state) => state.camera)
 
   const waterRef = useRef<GerstnerWater>(null)
+  const planksRef = useRef<Object3D>()
 
   useLayoutEffect(() => {
     ship.position.set(2, 2, 2)
-    ship.add(camera)
+
+    ship.traverse((child) => {
+      if (child.name === WALKING_SURFACE) {
+        planksRef.current = child
+      }
+    })
+    if (onBoardCamera) ship.add(camera)
   }, [ship]) // eslint-disable-line
 
   useFrame((state, delta) => {
     const { forward, backward } = get()
 
-    waterRef.current!.update(delta)
+    waterRef.current!.update(delta * 0.4)
 
     const t = waterRef.current!.material.uniforms['time'].value
 
@@ -42,11 +58,19 @@ export const WaterScene = ({ sunDirection }: Props) => {
     const quat = new Quaternion().setFromEuler(
       new Euler(waveInfo.normal.x, waveInfo.normal.y, waveInfo.normal.z)
     )
-    ship.quaternion.rotateTowards(quat, delta * 0.5)
+    ship.quaternion.rotateTowards(quat, delta)
+
+    if (!onBoardCamera) return
+
+    raycaster.set(camera.getWorldPosition(worldPosition), raycasterDirection)
+    const [intersection] = raycaster.intersectObject(planksRef.current!)
 
     if (forward) {
       getCameraLocalDirection(camera, localDirection)
-      camera.position.add(localDirection)
+
+      camera.position.x += localDirection.x
+      camera.position.y -= intersection.distance - CHARACTER_HEIGHT
+      camera.position.z += localDirection.z
     } else if (backward) {
       getCameraLocalDirection(camera, localDirection)
       camera.position.sub(localDirection)
